80d14e26f2a5b3ff643fe03b92ca0ce531515a82
Ankit Kumar Mistry <ankitkmistry@outlook.com>
2026-02-02 12:32:32 +0530

    Remove smear cursor because it is buggy

--- lua/plugins/helpful.lua
+++ lua/plugins/helpful.lua
@@ -20,21 +20,6 @@ return {
 		opts = {},
 	},
 	{
-		"sphamba/smear-cursor.nvim",
-		-- Disable smear-cursor in neovide
-		-- cond = vim.g.neovide == nil,
-		cond = vim.g.neovide == nil,
-		opts = {
-			stiffness = 0.8, -- 0.6      [0, 1]
-			trailing_stiffness = 0.6, -- 0.45     [0, 1]
-			stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-			trailing_stiffness_insert_mode = 0.7, -- 0.5      [0, 1]
-			damping = 0.95, -- 0.85     [0, 1]
-			damping_insert_mode = 0.95, -- 0.9      [0, 1]
-			distance_stop_animating = 0.5, -- 0.1      > 0
-		},
-	},
-	{
 		"folke/todo-comments.nvim",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		opts = {},
