9353c61196349b36ac9870eaa41c3535919e8fd1
Ankit Kumar Mistry <ankitkmistry@outlook.com>
2026-02-17 18:55:59 +0530

    Add the modus themes to the config

--- lua/plugins/decoration.lua
+++ lua/plugins/decoration.lua
@@ -118,7 +118,7 @@ return {
 			require("tokyonight").setup({
 				transparent = false,
 			})
-			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-night")
 		end,
 	},
 	{
@@ -135,6 +135,51 @@ return {
 				},
 			})
 			-- vim.cmd.colorscheme("kanagawa")
+		end,
+	},
+	{
+		"miikanissi/modus-themes.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("modus-themes").setup({
+				-- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+				-- `auto` will automatically set style based on background set with vim.o.background
+				style = "auto",
+                -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+				variant = "tinted",
+                -- Transparent background (as supported by the terminal)
+				transparent = true,
+                -- "non-current" windows are dimmed
+				dim_inactive = false,
+                -- Hide statuslines on inactive windows. Works with the standard **StatusLine**, **LuaLine** and **mini.statusline**
+				hide_inactive_statusline = false,
+                -- Distinct background colors in line number column. `false` will disable background color and fallback to Normal background
+				line_nr_column_background = true,
+                -- Distinct background colors in sign column. `false` will disable background color and fallback to Normal background
+				sign_column_background = true,
+                -- Style to be applied to different syntax groups
+                -- Value is any valid attr-list value for `:help nvim_set_hl`
+				styles = {
+					comments = { italic = true },
+					keywords = { italic = true },
+					functions = {},
+					variables = {},
+				},
+				--- You can override specific color groups to use other groups or a hex color
+				--- Function will be called with a ColorScheme table
+				--- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the ColorScheme table
+				---@param colors ColorScheme
+				on_colors = function(colors) end,
+				--- You can override specific highlights to use other groups or a hex color
+				--- Function will be called with a Highlights and ColorScheme table
+				--- Refer to `extras/lua/modus_operandi.lua` or `extras/lua/modus_vivendi.lua` for the Highlights and ColorScheme table
+				---@param highlights Highlights
+				---@param colors ColorScheme
+				on_highlights = function(highlights, colors) end,
+			})
+            -- Activate the theme
+			vim.cmd.colorscheme("modus")
 		end,
 	},
 	{
